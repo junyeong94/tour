b4974f203e69a8e616c14ce92db35d4fd8652b1a
junyeong94 <64447875+junyeong94@users.noreply.github.com>
2020-05-28 18:00:40 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,45 @@
 $(function(){
-    
+   fnKakao();
+   fnmainNav();
+   // 스킵메뉴
+    $('.skip>a').focus(function(){
+       $('.skip').addClass('active'); 
+    });
+    $('.skip>a').blur(function(){
+        $('.skip').removeClass('active'); 
+     });      
+   // 1066px 이상에서의 pos_fix 카카오톡 변경
+   $(window).resize(function(){
+      fnKakao();
+      fnmainNav();
+   });
+
+   function fnKakao(){
+      var winWidth=$(this).width();
+      if(winWidth>=1066){
+         $('#kakao').text('카카오톡');
+      }else{
+         $('#kakao').text('카카오톡 상담');
+      }
+   }
+   // 헤더 토글 네비게이션
+   $('.tgl_btn').click(function(){
+      $('.nav_box').slideToggle();
+      $('#header').css('background-color','#3a3e46');
+   });
+
+   // 메인 네비 리사이즈
+   function fnmainNav(){
+      var winWidth=$(window).width();
+      if(winWidth>=801){
+         $('.nav_box').show();
+      }else{
+         $('.nav_box').hide();
+      }
+   }
+
+   // 메인슬라이더
+   var mySwiper = new Swiper('.swiper-container', {
+      speed: 400,
+  });
 });

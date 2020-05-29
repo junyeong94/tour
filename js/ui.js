62a7fca4f52e1fada73a2ddf99462d3bbb111c98
junyeong94 <64447875+junyeong94@users.noreply.github.com>
2020-05-29 18:01:33 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,7 @@
 $(function(){
    fnKakao();
    fnmainNav();
+   gridSystem()
    // 스킵메뉴
     $('.skip>a').focus(function(){
        $('.skip').addClass('active'); 
@@ -39,7 +40,65 @@ $(function(){
    }
 
    // 메인슬라이더
-   var mySwiper = new Swiper('.swiper-container', {
+   var mySwiper = new Swiper('.mo_banner,.tab_banner', {
       speed: 400,
+      loop:true,
+      autoplay:{
+         delay:3000,
+      },
+      pagination: {
+         el: '.swiper-pagination',
+       },
   });
+
+//   슬라이더 멈춤/재생
+  var stopPlay=$('.tab_banner .swiper-pagination::after');
+     
+
+//   스크롤이벤트
+  $(window).scroll(function(){
+     var winTop=$(window).scrollTop();
+     var winWidth=$(window).width();
+   //   모바일조건
+     if(winTop>=200 && winWidth<=1065){
+        $('.pos_fix>p').css('display','block');
+     }else if(winTop<200 && winWidth<=1065){
+         $('.pos_fix>p').hide();
+     }else if(winTop>=200 && winWidth>=1066){
+         $('.pos_fix>ul').show();
+         $('.pos_fix>p').css('display','block');
+     }else if(winTop<200 && winWidth>=1066){
+         $('.pos_fix>ul').hide();
+         $('.pos_fix>p').hide();
+     }
+  });
+
+//   윈도우 리사이즈 이벤트
+   $(window).resize(function(){
+      gridSystem();
+      var winWidth=$(window).width();
+      if(winWidth<=800){
+         $('.pos_fix>ul').show();
+      }else if(winWidth<=1066){
+         $('.pos_fix>ul').hide();
+      }
+   });
+
+
+//   스무스 스크롤
+   var scroll = new SmoothScroll('a[data-scroll]', {
+	   speed:500
+   });
+   
+   // 화면폭에 다른 그리드 적용
+   function gridSystem(){
+   var winWidth=$(window).width();
+   if(winWidth<=800){
+      // 모바일영역
+      $('.top3>li').removeClass('pc_g4');
+   }else{
+      // 태블릿영역
+      $('.top3>li').addClass('pc_g4');
+   }
+}
 });
